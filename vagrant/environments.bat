06203590686347103db9a82450aaeb510cce15ca
william1605 <will.especialista@gmail.com>
2018-11-13 08:32:35 -0400

    Files Vagrantfile and enviroments added

---
+++ vagrant/environments.bat
@@ -0,0 +1,12 @@
+@echo off
+set OS_AUTH_URL=http://10.28.3.240:5000/v3
+set OS_IDENTITY_API_VERSION=3
+set OS_TENANT_NAME=8dbcca4b5ab744acbd5c7c3dc1d388fe
+set OS_PROJECT_NAME=fj_williamclaros_at07-desktop
+set OS_USERNAME=WilliamClaros
+set OS_PASSWORD=Qwertyuiop123456789
+set OS_DOMAIN_NAME=fundacion-jala
+set OS_FLAVOR=f1.desktop.small
+set OS_IMAGE=ubuntu-18.04-amd64-server_18062018
+set OS_KEY_PAIR_NAME=williamKey
+set OS_REGION_NAME=bootstack-jalasoft
