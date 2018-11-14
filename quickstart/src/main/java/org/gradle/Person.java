049a14119d7f39b89417c01404bfb1cb7b636437
william1605 <will.especialista@gmail.com>
2018-11-14 17:08:46 -0400

    Addition of Test Cases and modification of Vagrantfile

--- quickstart/src/main/java/org/gradle/Person.java
+++ quickstart/src/main/java/org/gradle/Person.java
@@ -3,14 +3,56 @@ package org.gradle;
 import org.apache.commons.collections.list.GrowthList;
 
 public class Person {
+	
     private final String name;
+    private final String gender;
+    private final int ci;
+    private String address;
+    private int age;
+    private int phone;
 
-    public Person(String name) {
+    public Person(String name,String gender, int ci, String address, int age, int phone) {
         this.name = name;
-        new GrowthList();
+        this.gender = gender;
+        this.ci = ci;
+        this.address = address;
+        this.age = age;
+        this.phone = phone;
     }
 
     public String getName() {
         return name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public int getCi() {
+        return ci;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address){
+    	this.address = address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age){
+    	this.age = age;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone){
+    	this.phone = phone;
     }
 }
